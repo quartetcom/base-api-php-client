4271c2a200b57bcee4aabd80f2318ee533daa63b
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-12-08 19:51:31 +0900

    simplify demo

--- demo/index.php
+++ demo/index.php
@@ -6,21 +6,25 @@ use Quartet\BaseApi\Api\Items;
 use Quartet\BaseApi\Api\Users;
 use Quartet\BaseApi\Client;
 
-$client = new Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, ['read_users', 'read_items']);
+$scopes = [
+    'read_users',
+    'read_items',
+];
+
+$client = new Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, $scopes);
 
 if (isset($_GET['code'])) {
     $client->authenticate($_GET['code']);
-} elseif (isset($_GET['error']) && isset($_GET['error_description'])) {
-    echo "[{$_GET['error']}] {$_GET['error_description']}";
-    exit;
 } else {
     $client->authorize();
 }
 
+// Users api.
 $usersApi = new Users($client);
 $user = $usersApi->me();
 var_dump($user->shop_name);
 
+// Items api.
 $itemsApi = new Items($client);
 $items = $itemsApi->get();
 foreach ($items as $item) {
