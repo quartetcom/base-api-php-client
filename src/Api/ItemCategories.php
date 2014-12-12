047fc78567f99d86de3bfea914ec6f42070b2e7b
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-12-12 11:50:58 +0900

    add ItemCategories api

--- src/Api/ItemCategories.php
+++ src/Api/ItemCategories.php
@@ -1,17 +1,71 @@
 <?php
 namespace Quartet\BaseApi\Api;
 
-class ItemCategories
+use Quartet\BaseApi\Entity\ItemCategory;
+
+class ItemCategories extends Api
 {
-    public function detail()
+    /**
+     * @param int|string $item_id
+     * @return \Quartet\BaseApi\Entity\ItemCategory[]
+     */
+    public function detail($item_id)
     {
+        $response = $this->client->request('post', "/1/item_categories/detail/{$item_id}");
+
+        $data = json_decode($response->getBody(), true);
+
+        $itemCategories = [];
+        foreach ($data['item_categories'] as $itemCategory) {
+            $itemCategories[] = $this->entityManager->getEntity('ItemCategory', $itemCategory);
+        }
+
+        return $itemCategories;
     }
 
-    public function add()
+    /**
+     * @param \Quartet\BaseApi\Entity\ItemCategory $itemCategory
+     * @return \Quartet\BaseApi\Entity\ItemCategory[]
+     * @throws MissingRequiredParameterException
+     */
+    public function add(ItemCategory $itemCategory)
     {
+        if (is_null($itemCategory->item_id)) {
+            throw new MissingRequiredParameterException;
+        }
+
+        $params = $this->entityManager->getFlatArray($itemCategory);
+
+        $response = $this->client->request('post', '/1/item_categories/add', $params);
+
+        $data = json_decode($response->getBody(), true);
+
+        $itemCategories = [];
+        foreach ($data['item_categories'] as $itemCategory) {
+            $itemCategories[] = $this->entityManager->getEntity('Category', $itemCategory);
+        }
+
+        return $itemCategories;
+
     }
 
-    public function delete()
+    /**
+     * @param int|string $item_category_id
+     * @return \Quartet\BaseApi\Entity\ItemCategory[]
+     */
+    public function delete($item_category_id)
     {
+        $response = $this->client->request('post', '/1/item_categories/delete', [
+            'item_category_id' => $item_category_id,
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        $itemCategories = [];
+        foreach ($data['item_categories'] as $itemCategory) {
+            $itemCategories[] = $this->entityManager->getEntity('Category', $itemCategory);
+        }
+
+        return $itemCategories;
     }
 }
---
+++ src/Entity/ItemCategory.php
@@ -0,0 +1,9 @@
+<?php
+namespace Quartet\BaseApi\Entity;
+
+class ItemCategory implements EntityInterface
+{
+    public $item_category_id;
+    public $item_id;
+    public $category_id;
+}
