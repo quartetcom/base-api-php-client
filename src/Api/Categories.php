d05b2937675f06800c1c26084eee0de7f8147943
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-12-12 11:41:02 +0900

    add Categories api

--- src/Api/Categories.php
+++ src/Api/Categories.php
@@ -1,21 +1,94 @@
 <?php
 namespace Quartet\BaseApi\Api;
 
-class Categories
+use Quartet\BaseApi\Entity\Category;
+
+class Categories extends Api
 {
+    /**
+     * @return \Quartet\BaseApi\Entity\Category[]
+     */
     public function get()
     {
+        $response = $this->client->request('get', '/1/categories');
+
+        $data = json_decode($response->getBody(), true);
+
+        $categories = [];
+        foreach ($data['categories'] as $category) {
+            $categories[] = $this->entityManager->getEntity('Category', $category);
+        }
+
+        return $categories;
     }
 
-    public function add()
+    /**
+     * @param \Quartet\BaseApi\Entity\Category $category
+     * @return \Quartet\BaseApi\Entity\Category[]
+     * @throws MissingRequiredParameterException
+     */
+    public function add(Category $category)
     {
+        if (is_null($category->name)) {
+            throw new MissingRequiredParameterException;
+        }
+
+        $params = $this->entityManager->getFlatArray($category);
+
+        $response = $this->client->request('post', '/1/categories/add', $params);
+
+        $data = json_decode($response->getBody(), true);
+
+        $categories = [];
+        foreach ($data['categories'] as $category) {
+            $categories[] = $this->entityManager->getEntity('Category', $category);
+        }
+
+        return $categories;
     }
 
-    public function edit()
+    /**
+     * @param \Quartet\BaseApi\Entity\Category $category
+     * @return \Quartet\BaseApi\Entity\Category[]
+     * @throws MissingRequiredParameterException
+     */
+    public function edit(Category $category)
     {
+        if (is_null($category->category_id)) {
+            throw new MissingRequiredParameterException;
+        }
+
+        $params = $this->entityManager->getFlatArray($category);
+
+        $response = $this->client->request('post', '/1/categories/edit', $params);
+
+        $data = json_decode($response->getBody(), true);
+
+        $categories = [];
+        foreach ($data['categories'] as $category) {
+            $categories[] = $this->entityManager->getEntity('Category', $category);
+        }
+
+        return $categories;
     }
 
-    public function delete()
+    /**
+     * @param int|string $category_id
+     * @return \Quartet\BaseApi\Entity\Category[]
+     */
+    public function delete($category_id)
     {
+        $response = $this->client->request('post', '/1/categories/delete', [
+            'category_id' => $category_id,
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        $categories = [];
+        foreach ($data['categories'] as $category) {
+            $categories[] = $this->entityManager->getEntity('Category', $category);
+        }
+
+        return $categories;
     }
 }
---
+++ src/Entity/Category.php
@@ -0,0 +1,9 @@
+<?php
+namespace Quartet\BaseApi\Entity;
+
+class Category implements EntityInterface
+{
+    public $category_id;
+    public $name;
+    public $list_order;
+}
